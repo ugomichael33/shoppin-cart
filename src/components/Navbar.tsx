5f84f44e2abba491ef1982141b7aaade0510c092
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-11-23 10:18:05 +0100

    store page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
-import { Navbar as NavbarBS, Container, Nav } from "react-bootstrap"
+import { Navbar as NavbarBS, Container, Nav, Button } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import { Badge, IconButton } from "@mui/material"
+
+
 
 export function Navbar(){
-    return <NavbarBS className='bg-white shadow-sm mb-3 p-3'>
+    return <NavbarBS sticky='top' className='bg-white shadow-sm mb-3 p-3'>
                 <Container>
                     <Nav className="me-auto">
                         <Nav.Link to={'/'} as={NavLink}>Home</Nav.Link>
                         <Nav.Link to={'/store'} as={NavLink}>Store</Nav.Link>
                         <Nav.Link to={'/about'} as={NavLink}>About</Nav.Link>
                     </Nav>
-                        Hi
+                    <IconButton>
+                        <Badge color="error" badgeContent={1}>
+                            <AddShoppingCartIcon />
+                        </Badge>
+                    </IconButton>
                 </Container>
            </NavbarBS>
 }
