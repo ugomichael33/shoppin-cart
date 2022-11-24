2c7aaa9b41676405e260983f022299cc04f4b880
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-11-24 12:12:37 +0100

    added total func

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,6 +2,8 @@ import { Stack } from "react-bootstrap";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CartItem } from './CartItem'
+import { formatCurrency } from "../utilities/formatCurrency"
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps = {
     isOpen: boolean
@@ -19,6 +21,15 @@ export function ShoppingCart({isOpen}: ShoppingCartProps) {
                         {cartItems.map(item => (
                             <CartItem key={item.id} {...item} />
                         ))}
+                        <div className="ms-auto fw-bold fs-5">
+                            Total{" "}
+                            {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItems.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0) * cartItem.quantity
+                            }, 0)
+                            )}
+                        </div>
                     </Stack>
                 </Offcanvas.Body>
             </Offcanvas>
